cca1126d1287c7e2afd4a827215e9a784c2dc32a
creativedebliteck <nicodebliteck@gmail.com>
2018-06-26 16:35:08 +0530

    update code

--- test.php
+++ test.php
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
 <? 
 echo "edit by ashish";
 echo "test git";
-<? echo "test git";
+
 
 echo "hello";
 
 
 echo "test open in same";
-<<<<<<< HEAD
-=======
 echo "new line";
->>>>>>> 4ff969e04e65049e89500700957c02bfdd35930b
 ?>
