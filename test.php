240fffe813e8d2e23e4da8a04f83f2c7997a32e8
nicodebliteck <nico@debliteck.com>
2018-06-27 12:21:48 +0530

    line added in test.php

--- test.php
+++ test.php
@@ -1,20 +1,16 @@
 <? 
-echo "edit by ashish";
-echo "test again";
-echo "test git";
+echo 'edit by ashish';
+echo 'test again';
+echo 'test git';
 
-echo "test new line";
-echo "hello";
+echo 'test new line';
+echo 'hello';
 echo 'added line in ubantu';
 
-echo "test open in same";
+echo 'test open in same';
 
-echo "asdasdasdasdasd asdasdasd";
-echo "new line";
-echo "newline";
-<<<<<<< HEAD
+echo 'asdasdasdasdasd asdasdasd';
+echo 'new line';
+echo 'newline';
 echo 'newline again';
-=======
-echo "newline again";
->>>>>>> 0366bb8d05566eecb45906b8a8a26def82ad1046
 ?>
